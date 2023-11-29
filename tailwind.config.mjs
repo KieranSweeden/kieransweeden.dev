f8cbe39a6b6014047dbaf9eb070d4016d05093fb
Kieran Sweeden <kieransweeden@gmail.com>
2023-11-29 00:12:39 +0000

    Locally include & apply petrona font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {},
+        fontFamily: {
+            petrona: ["Petrona", "serif"],
+        },
     },
     plugins: [],
 };
