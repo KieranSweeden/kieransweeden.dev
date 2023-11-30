12032059f3a07271a6e82ce1ddda8aac95d4176f
Kieran Sweeden <kieransweeden@gmail.com>
2023-11-30 21:18:06 +0000

    add meta image

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Kieran Sweeden";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "kieran sweeden";
+export const SITE_DESCRIPTION = "A personal site authored by Kieran Sweeden";
